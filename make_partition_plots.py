384579139ddfa4f40908f7fb0485d288960ca95a
Danny Hermes <daniel.j.hermes@gmail.com>
2015-02-06 12:18:52 -0800

    Adding papers, allowing 1D to have N_exp as param.
    Also adding cardiod example FFT to give something we can visualize.

--- make_partition_plots.py
+++ make_partition_plots.py
@@ -8,6 +8,7 @@ N = 2**N_exp
 
 def remove_axis_frame(ax):
     ax.set_frame_on(False)
+    ax.yaxis.set_label_position('right')
     ax.set_xticklabels([])
     # H/T: http://stackoverflow.com/a/20416681/1068170
     for tic in ax.xaxis.get_major_ticks():
@@ -17,7 +18,8 @@ def remove_axis_frame(ax):
         tic.tick1On = tic.tick2On = False
 
 
-def make_1D_ax_just_s(ax, s_size):
+def make_1D_ax_just_s(ax, s_size, N_exp=N_exp):
+    N = 2**N_exp
     all_s = np.linspace(0, N, s_size + 1)
     ax.plot(all_s, 0.5 * N * np.ones(all_s.shape),
             color='b', marker='o')
@@ -27,7 +29,8 @@ def make_1D_ax_just_s(ax, s_size):
     ax.set_ylim(-1, N + 1)
 
 
-def make_1D_ax_just_t(ax, t_size):
+def make_1D_ax_just_t(ax, t_size, N_exp=N_exp):
+    N = 2**N_exp
     all_t = np.linspace(0, N, t_size + 1)
     ax.plot(0.5 * N * np.ones(all_t.shape), all_t,
             color='b', marker='o')
@@ -37,9 +40,10 @@ def make_1D_ax_just_t(ax, t_size):
     ax.set_ylim(-1, N + 1)
 
 
-def make_1D_ax_both(ax, ell, t_size, s_size):
+def make_1D_ax_both(ax, ell, t_size, s_size, N_exp=N_exp):
     ax.set_title(r'$L = %d, \, \ell = %d$' % (N_exp, ell))
 
+    N = 2**N_exp
     all_s, all_t = np.linspace(0, N, s_size + 1), np.linspace(0, N, t_size + 1)
     # Make all horizontal lines.
     for t in all_t:
@@ -58,14 +62,14 @@ def make_1D_ax_both(ax, ell, t_size, s_size):
     ax.set_ylim(-1, N + 1)
 
 
-def make_1D_example_plot(ell):
+def make_1D_example_plot(ell, N_exp=N_exp):
     rows, cols = 1, 3
     fig, (ax1, ax2, ax3) = plt.subplots(rows, cols)
 
     t_size, s_size = 2**ell, 2**(N_exp - ell)
-    make_1D_ax_just_s(ax1, s_size)
-    make_1D_ax_both(ax2, ell, t_size, s_size)
-    make_1D_ax_just_t(ax3, t_size)
+    make_1D_ax_just_s(ax1, s_size, N_exp=N_exp)
+    make_1D_ax_both(ax2, ell, t_size, s_size, N_exp=N_exp)
+    make_1D_ax_just_t(ax3, t_size, N_exp=N_exp)
 
     width, height = fig.get_size_inches()
     fig.set_size_inches(2 * width, 2 * height, forward=True)
