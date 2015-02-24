f0348a6b9f314298dcb50fd092865362d4809d85
Danny Hermes <daniel.j.hermes@gmail.com>
2015-02-24 01:26:38 -0800

    Running naive (quadratic) DFT and checking against the FFT in Go.

--- golang/butterfly.go
+++ golang/butterfly.go
@@ -4,16 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"math/cmplx"
 	"path"
 	"runtime"
 
 	"github.com/gonum/blas/cblas128"
+	"github.com/mjibson/go-dsp/fft"
 )
 
 const minusI complex128 = complex(0, -1)
 
-func LoadWhale() (*[]float64, error) {
+func LoadWhale() (*[]complex128, error) {
 	// H/T: andrewbrookins.com/tech/golang-get-directory-of-the-current-file/
 	_, filename, _, _ := runtime.Caller(1)
 	whale_path := path.Join(path.Dir(filename), "bluewhale.json")
@@ -23,8 +25,13 @@ func LoadWhale() (*[]float64, error) {
 		return nil, err
 	}
 
-	var blue_whale_call []float64
-	json.Unmarshal(contents, &blue_whale_call)
+	var float64_call []float64
+	json.Unmarshal(contents, &float64_call)
+
+	blue_whale_call := make([]complex128, len(float64_call))
+	for i, value := range float64_call {
+		blue_whale_call[i] = complex(value, 0.0)
+	}
 	return &blue_whale_call, nil
 }
 
@@ -52,31 +59,42 @@ func computeFHat(f, t, s []complex128) []complex128 {
 	f_vec := cblas128.Vector{Inc: 1, Data: f}
 	for k := 0; k < N; k++ {
 		v := cblas128.Vector{Inc: 1, Data: dftKernelVectorized(t[k], s)}
-		fmt.Println("v.Data:", v.Data)
 		f_hat[k] = cblas128.Dotu(N, v, f_vec)
 	}
 	return f_hat
+}
+
+func getDFTData(N int) ([]complex128, []complex128) {
+	t := make([]complex128, N)
+	s := make([]complex128, N)
+	for k := 0; k < N; k++ {
+		t[k] = 2.0 * math.Pi * complex(float64(k), 0)
+		s[k] = complex(float64(k)/float64(N), 0)
+	}
+	return t, s
 }
 
 func main() {
 	// See: http://godoc.org/github.com/gonum/blas
 	// sudo apt-get install libopenblas-dev
 	// CGO_LDFLAGS="-L/usr/lib/libopenblas.so -lopenblas" go install github.com/gonum/blas/cgo
-	v := cblas128.Vector{Inc: 1, Data: []complex128{1, 1, 1}}
-	fmt.Println("v has length:", cblas128.Nrm2(len(v.Data), v))
-
-	t := []complex128{1.0, 2.0}
-	s := []complex128{3.0, 4.0}
-	f := []complex128{5.0, 6.0}
-	fmt.Println("t:", t)
-	fmt.Println("s:", s)
-	fmt.Println("f:", f)
-	fmt.Println("fhat:", computeFHat(f, t, s))
-
 	blue_whale_call, err := LoadWhale()
 	if err != nil {
 		fmt.Printf("File error: %v\n", err)
-	} else {
-		fmt.Printf("Number of values: %v\n", len(*blue_whale_call))
 	}
+
+	N := len(*blue_whale_call)
+	t, s := getDFTData(N)
+
+	dft_whale_call := fft.FFT(*blue_whale_call)
+	computed_f_hat := computeFHat(*blue_whale_call, t, s)
+
+	fmt.Println("N:", N)
+
+	err_vals := cblas128.Vector{Inc: 1, Data: dft_whale_call}
+	c := cblas128.Vector{Inc: 1, Data: computed_f_hat}
+	// err_vals -= computed_f_hat
+	cblas128.Axpy(N, -1.0, c, err_vals)
+
+	fmt.Println("||e||_2:", cblas128.Nrm2(N, err_vals))
 }
